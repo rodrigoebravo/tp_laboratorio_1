0b4a76a39e5b5ba6b371426486a3bf3f744ef5f8
Rodrigo <rodrigobravo-@hotmail.com>
2018-09-04 05:24:35 -0300

    Solo faltan documentar las funciones

--- calc.c
+++ calc.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 int getFloat(float* pResultado);
+int esEntero(float NumeroIngresado);
+
 int calc_getSuma(float numeroUno, float numeroDos, float* resultado)
 {
     int retorno=0;
@@ -28,94 +30,103 @@ int calc_getMultiplicacion(float numeroUno, float numeroDos, float* resultado)
 }
 int calc_getDivision(float numeroUno, float numeroDos, float* resultado)
 {
-    int retorno=0;
+    int retorno=-1;
     float auxDivision=0;
 
-    if(numeroDos==0)
-    {
-        retorno=-1;
-    }
-    else
+    if(numeroDos!=0)
     {
         auxDivision=numeroUno/numeroDos;
         *resultado=auxDivision;
+        retorno=0;
     }
-
     return retorno;
 }
 
-int calc_getFactorial(float numeroUno, float numeroDos, int* resultadoA, int* resultadoB)
+int calc_getFactorial(float numero, int unsigned long* resultado)
 {
-    int retorno=0;
-    int auxResultadoA=numeroUno;
-    float auxNumeroUno;
-    int auxResultadoB=numeroDos;
-    float auxNumeroDos;
+    int retorno=-1;
+    int unsigned long auxResultado=numero;
+
     int i;
 
-    auxNumeroUno=(int)numeroUno-numeroUno;
-    if(auxNumeroUno==0)
+    if(esEntero(numero)==0)
     {
-        for(i=auxResultadoA-1;i>1;i--)
+        if(numero==0)
         {
-            auxResultadoA*=i;
-
+            auxResultado=1;
+            retorno=0;
         }
-        *resultadoA=auxResultadoA;
-    }
-    else
-    {
-        retorno=-1;
-    }
-    auxNumeroDos=(int)numeroDos-numeroDos;
-    if(auxNumeroDos==0)
-    {
-        for(i=auxResultadoB-1;i>1;i--)
+        else if(numero>0)
         {
-            auxResultadoB*=i;
-
+            for(i=auxResultado-1;i>1;i--)
+            {
+                auxResultado*=i;
+            }
+            retorno=0;
         }
-        *resultadoB=auxResultadoB;
+        else
+        {
+            auxResultado=0;
+            retorno=-2;
+        }
+        *resultado=auxResultado;
     }
-    else
-    {
-        retorno=-1;
-    }
-
 
     return retorno;
 }
 
-int calc_getEntero(float* pNumeroIngresado, int reintentos, char* mensaje, char* mensajeError)
+int esEntero(float NumeroIngresado)
 {
+    float esEntero;
+    int retorno=-1;
+    esEntero=(int)NumeroIngresado-NumeroIngresado;
 
-    return 0;
+    if(esEntero==0)
+    {
+        retorno=0;
+    }
+    return retorno;
 }
 
 int calc_getDecimal(float* pDecimal, int reintentos, char* mensaje, char* mensajeError)
 {
     int retorno=-1;
     float auxiliarDecimal;
-    for(;reintentos>0;reintentos--)
+    if(reintentos!=0)
     {
+        for(;reintentos>0;reintentos--)
+        {
 
-        printf(mensaje);
-        if(getFloat(&auxiliarDecimal)==0)
-        {
-            *pDecimal=auxiliarDecimal;
-            retorno=0;
-            system("cls");
-            break;
-        }
-        else
-        {
-            system("cls");
-            printf(mensajeError);
-            fflush(stdin);
+
+            if(getFloat(&auxiliarDecimal)==0)
+            {
+                *pDecimal=auxiliarDecimal;
+                retorno=0;
+                system("cls");
+                break;
+            }
+            else
+            {
+                system("cls");
+                printf(mensajeError);
+                fflush(stdin);
+            }
         }
     }
+    else
+    {   printf(mensaje);
+        while(scanf("%f",&auxiliarDecimal)!=1)
+        {
 
-
+            system("cls");
+            printf(mensajeError);
+            printf(mensaje);
+            fflush(stdin);
+        }
+        *pDecimal=auxiliarDecimal;
+        retorno=0;
+        system("cls");
+    }
     return retorno;
 }
 
@@ -131,26 +142,26 @@ int getFloat(float* pResultado)
     return retorno;
 }
 
-void calc_mostrarMenu(float valorIngresadoUno, float valorIngresadoDos, int* contadorMenu, int* respuesta)
+void calc_mostrarMenu(float valorIngresadoUno, float valorIngresadoDos, int ingresoA, int ingresoB, int* respuesta)
 {
     int auxRespuesta=0;
 
-    if(*contadorMenu==0)
+    if(ingresoA==0)
     {
         printf("1. Ingresar 1er operando (A=x)\n");
     }
     else
     {
-        printf("1. Ingresar 1er operando (A=%f)\n", valorIngresadoUno);
+        printf("1. Ingresar 1er operando (A=%.2f)\n", valorIngresadoUno);
     }
 
-    if(*contadorMenu!=2)
+    if(ingresoB==0)
     {
         printf("2. Ingresar 2do operando (B=y)\n");
     }
     else
     {
-        printf("2. Ingresar 2do operando (B=%f)\n", valorIngresadoDos);
+        printf("2. Ingresar 2do operando (B=%.2f)\n", valorIngresadoDos);
     }
 
     printf("3. Calcular todas las operaciones\n");
--- main.c
+++ main.c
@@ -15,20 +15,24 @@ int main()
     float resultadoResta;
     float resultadoMultiplicacion;
     float resultadoDivision;
-    int resultadoFactorialA;
-    int resultadoFactorialB;
-    int contadorResp;
+    int unsigned long resultadoFactorialA;
+    int unsigned long resultadoFactorialB;
     int respuesta;
+    int banderaIngresoA=0;
+    int banderaIngresoB=0;
+    int banderaCalculo=0;
+    int divisionOk;
+    int factorialAOk;
+    int factorialBOk;
 
     mensaje="Ingrese numero: ";
     mensajeError="El valor ingresado no es correcto. \n";
-    reintentos=3;
+    reintentos=0;
 
 
 
     do{
-        calc_mostrarMenu(valorIngresadoUno,valorIngresadoDos,&contadorResp,&respuesta);
-        contadorResp++;
+        calc_mostrarMenu(valorIngresadoUno,valorIngresadoDos,banderaIngresoA,banderaIngresoB,&respuesta);
 
         switch(respuesta)
         {
@@ -37,34 +41,102 @@ int main()
                 {
                     system("cls");
                     printf("Ha agotado los reintentos posibles\n");
+                    banderaIngresoA=0;
+                    break;
                 }
+                banderaIngresoA=1;
                 break;
             case 2:
                 if(calc_getDecimal(&valorIngresadoDos, reintentos, mensaje, mensajeError)!=0)
                 {
                     system("cls");
                     printf("Ha agotado los reintentos posibles\n");
+                    banderaIngresoB=0;
+                    break;
                 }
+                banderaIngresoB=1;
                 break;
 
             case 3:
+                if(banderaIngresoA!=1 || banderaIngresoB!=1)
+                {
+                    printf("Debe ingresar valores de A y B antes de calcular.\n");
+                    break;
+                }
                 calc_getSuma(valorIngresadoUno,valorIngresadoDos,&resultadoSuma);
                 calc_getResta(valorIngresadoUno,valorIngresadoDos,&resultadoResta);
                 calc_getMultiplicacion(valorIngresadoUno,valorIngresadoDos,&resultadoMultiplicacion);
-                calc_getDivision(valorIngresadoUno,valorIngresadoDos,&resultadoDivision);
-                calc_getFactorial(valorIngresadoUno, valorIngresadoDos,&resultadoFactorialA,&resultadoFactorialB);
+                divisionOk=calc_getDivision(valorIngresadoUno,valorIngresadoDos,&resultadoDivision);
+                factorialAOk=calc_getFactorial(valorIngresadoUno,&resultadoFactorialA);
+                factorialBOk=calc_getFactorial(valorIngresadoDos,&resultadoFactorialB);
+                banderaCalculo=1;
                 break;
+
             case 4:
-                printf("A= %f\tB= %f\n", valorIngresadoUno, valorIngresadoDos);
-                printf("El resultado de A+B es: %f\n", resultadoSuma);
-                printf("El resultado de A-B es: %f\n", resultadoResta);
-                printf("El resultado de A/B es: %f\n", resultadoDivision);
-                printf("El resultado de A*B es: %f\n", resultadoMultiplicacion);
-                printf("El factorial de A es: %d y El factorial de B es: %d \n",resultadoFactorialA, resultadoFactorialB);
-                respuesta=5;
-                break;
+                if(banderaIngresoA!=1 || banderaIngresoB!=1)
+                {
+                    printf("No se pueden mostrar resultados, no se ingresaron valores para A y B.\n");
+                    break;
+                }
+                else
+                {
+                    if(banderaCalculo!=1)
+                    {
+                        printf("No se efectuaron los calculos, debe ingresar la opcion 3.\n");
+                        break;
+                    }
+
+                    printf("A= %.2f\tB= %.2f\n", valorIngresadoUno, valorIngresadoDos);
+                    printf("El resultado de A+B es: %.2f\n", resultadoSuma);
+                    printf("El resultado de A-B es: %.2f\n", resultadoResta);
+                    if(divisionOk==0)
+                    {
+                        printf("El resultado de A/B es: %.2f\n", resultadoDivision);
+                    }
+                    else
+                    {
+                        printf("No es posible dividir por cero\n");
+                    }
+                    printf("El resultado de A*B es: %.2f\n", resultadoMultiplicacion);
+                    //printf("El factorial de A es: %lu y El factorial de B es: %lu \n",resultadoFactorialA, resultadoFactorialB);
+                    if(factorialAOk==0)
+                    {
+                        printf("El factorial de A es: %lu",resultadoFactorialA);
+                    }
+                    else
+                    {
+                        if(factorialAOk==-1)
+                        {
+                            printf("No se pudo obtener el factorial de A, es decimal");
+                        }
+                        else
+                        {
+                            printf("No se pudo obtener el factorial de A, es negativo");
+                        }
+                    }
+                    if(factorialBOk==0)
+                    {
+                        printf(" y El factorial de B es: %lu",resultadoFactorialB);
+                    }
+                    else
+                    {
+                        if(factorialBOk==-1)
+                        {
+                            printf(", No se pudo obtener el factorial de B, es decimal.");
+                        }
+                        else
+                        {
+                            printf(", No se pudo obtener el factorial de B, es negativo.");
+                        }
+                    }
+                    respuesta=5;
+                    break;
+                }
+
             case 5:
+                printf("Hasta pronto!");
                 break;
+
             default:
                 printf("Opcion incorrecta! Reingrese opcion\n");
                 break;
